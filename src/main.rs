dd5277588f9840f63bcc4427501ed454dc54e353
asiryk <asiryk.net@gmail.com>
2022-08-05 19:48:14 +0300

    feat: use lazy static for symbols

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,20 @@ use serv_rs::toolbox::reel_area::{ReelSet, ReelStrip, Spinnable, Symbol};
 extern crate lazy_static;
 
 lazy_static! {
-    /// This is an example for using doc comment attributes
-    static ref SYM0: Symbol = Symbol::new(String::from("SYM0"));
+    // Initialize symbols.
+    pub static ref SYM0: Symbol = Symbol::new(String::from("SYM0"));
+    pub static ref SYM1: Symbol = Symbol::new(String::from("SYM1"));
+    pub static ref SYM2: Symbol = Symbol::new(String::from("SYM2"));
+    pub static ref SYM3: Symbol = Symbol::new(String::from("SYM3"));
+    pub static ref SYM4: Symbol = Symbol::new(String::from("SYM4"));
+    pub static ref SYM5: Symbol = Symbol::new(String::from("SYM5"));
 }
 
 fn main() {
-    let sym0 = Symbol::new(String::from("SYM0"));
-    let sym1 = Symbol::new(String::from("SYM1"));
-    let sym2 = Symbol::new(String::from("SYM2"));
-    let sym3 = Symbol::new(String::from("SYM3"));
-    let sym4 = Symbol::new(String::from("SYM4"));
-    let sym5 = Symbol::new(String::from("SYM5"));
-
     let mut reel_set = ReelSet::new(vec![
-        ReelStrip::new(vec![&sym0, &sym1, &sym2, &sym3, &sym4, &sym5], 3),
-        ReelStrip::new(vec![&sym0, &sym1, &sym2, &sym3, &sym4, &sym5], 3),
-        ReelStrip::new(vec![&sym0, &sym1, &sym2, &sym3, &sym4, &sym5], 3),
+        ReelStrip::new(vec![&*SYM0, &*SYM1, &*SYM2, &*SYM3, &*SYM4, &*SYM5], 3),
+        ReelStrip::new(vec![&*SYM0, &*SYM1, &*SYM2, &*SYM3, &*SYM4, &*SYM5], 3),
+        ReelStrip::new(vec![&*SYM0, &*SYM1, &*SYM2, &*SYM3, &*SYM4, &*SYM5], 3),
     ]);
 
     println!("{:?}", reel_set.get_visible_symbols());
@@ -27,6 +25,4 @@ fn main() {
     reel_set.spin();
 
     println!("\n\n{:?}", reel_set.get_visible_symbols());
-
-    println!("{}", &*SYM0);
 }
